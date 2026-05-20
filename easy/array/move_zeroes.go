a506d81b0e37e15ba935d3859137b6f22aa8119f
yuriy <yuriy.cherepnev@gmail.com>
2026-05-20 14:25:43 +0300

    5555

--- easy/array/move_zeroes.go
+++ easy/array/move_zeroes.go
@@ -1,25 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
-	digits := []int{0, 1, 0, 3, 12}
+	digits := []int{2, 1, 0, 3, 0, 12, 9, 67}
 
 	moveZeroes(digits)
 }
 
 func moveZeroes(nums []int) {
 	zeroIndex := 0
-	notZeroIndex := 0
 
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] == 0 {
-			zeroIndex = i
-		}
+	for i := 0; i < len(nums); i++ {
 		if nums[i] != 0 {
-			notZeroIndex = i
+			nums[zeroIndex], nums[i] = nums[i], nums[zeroIndex]
+			zeroIndex++
 		}
-		fmt.Println(zeroIndex, notZeroIndex)
-
 	}
 }
