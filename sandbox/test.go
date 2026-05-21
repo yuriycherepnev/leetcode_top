3a968f47e2b65005e9cc571ca9a4ea59b234f018
yuriy <yuriy.cherepnev@gmail.com>
2026-05-21 12:45:55 +0300

    666

--- sandbox/test.go
+++ sandbox/test.go
@@ -2,34 +2,10 @@ package main
 
 import "fmt"
 
-type User struct {
-	count int
-	book  Book
-}
-
-type Book struct {
-	count int
-}
-
-type UserInterface interface {
-	Inc()
-}
-
 func main() {
-	user := User{
-		count: 2,
-		book:  Book{count: 2},
-	}
+	fmt.Println('7')
+	fmt.Println('0')
 
-	user.book.Inc()
-
-	fmt.Println(user.book.count) // 1
-}
-
-func (u *User) Inc() {
-	u.count++
-}
-
-func (u *Book) Inc() {
-	u.count++
+	digit := int('7' - '0')
+	fmt.Println(digit)
 }
