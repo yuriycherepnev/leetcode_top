4c9cf0f0fcadbc25c4baa95beee2a78adbae0b91
yuriy <yuriy.cherepnev@gmail.com>
2026-06-08 12:43:06 +0300

    4444

--- concurency/sync_cond.go
+++ concurency/sync_cond.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var pokemonList = []string{"Charmander", "Squirtle", "Bulbasaur", "Jigglypuff"}
+var cond = sync.NewCond(&sync.Mutex{})
+var pokemon = ""
+
+func main() {
+	go func() {
+		cond.L.Lock()
+		defer cond.L.Unlock()
+
+		fmt.Println(pokemon)
+		for pokemon != "Pikachu" {
+			cond.Wait()
+		}
+		println("Caught" + pokemon)
+		pokemon = ""
+	}()
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			time.Sleep(time.Millisecond)
+
+			cond.L.Lock()
+			pokemon = pokemonList[rand.Intn(len(pokemonList))]
+			cond.L.Unlock()
+
+			cond.Signal()
+		}
+	}()
+
+	time.Sleep(100 * time.Millisecond) // lazy wait
+}
